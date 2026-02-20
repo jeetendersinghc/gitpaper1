cf69fb57e65f4a18ce5477ed48e969803413eb8b
Jeetender Singh Chauhan <jeetender.chauhan+merck@merck.com>
2026-02-20 16:07:56 -0500

    Version 2: Added extra dose

--- g-version/adex.r
+++ g-version/adex.r
@@ -1,5 +1,5 @@
 # ADEX demo script
-adex <- data.frame(ID = 1:5, DOSE = c(10, 20, 20, 10, 30))
+adex <- data.frame(ID = 1:6, DOSE = c(10, 20, 20, 10, 30, 40))
 total_dose <- sum(adex$DOSE)
 adex$DOSEGR <- ifelse(adex$DOSE > 15, "HIGH", "LOW")
 print(adex)
